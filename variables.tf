15b7be8a88daf8aaea933708fcc12d2d946bac00
AdnanAbubakarAdams <adnan_abubakaradams@yahoo.com>
2024-11-17 00:15:12 -0500

    testing with env vars

--- variables.tf
+++ variables.tf
@@ -4,13 +4,11 @@
 variable "zone_id" {
   type        = string
   description = "The zone ID for Cloudflare"
-    default = process.env.CLOUDFLARE_ZONE_ID
 }
 
 variable "account_id" {
   type        = string
   description = "The accout ID for Cloudflare"
-    default = process.env.CLOUDFLARE_ACCOUNT_ID
 }
 
 variable "domain_name" {
